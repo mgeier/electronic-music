3b673d6917d03c67a4dcacfe32b2108b4d682d7e
Matthias Geier <Matthias.Geier@gmail.com>
2014-05-06 14:46:00 +0200

    Minor changes

--- conf.py
+++ conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'A Practical Introduction to Electronic Music'
-copyright = '2013, Matthias Geier'
+copyright = '2013-2014, Matthias Geier'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- index.rst
+++ index.rst
@@ -1,9 +1,7 @@
-.. created by sphinx-quickstart on Wed Oct 30 10:29:50 2013.
-
 A Practical Introduction to Electronic Music
 ============================================
 
-This is just a placeholder. Hopefully, there is more to come ...
+This is work-in-progress. Hopefully, there is more to come ...
 
 .. toctree::
    :maxdepth: 2
